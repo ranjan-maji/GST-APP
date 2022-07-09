07c8ee4c0a1c5568e346db864234717388355a92
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2022-07-09 20:06:38 +0530

    2nd-try

--- middleware/validation.js
+++ middleware/validation.js
@@ -2,10 +2,10 @@ const joi = require("joi");
 
 function registerValidation(data) {
   const schema = joi.object({
-    firstName: joi.string().min(4).required(),
-    lastName: joi.string().min(4).required(),
-    email: joi.string().min(6).email().required(),
-    password: joi.string().min(4).required(),
+    firstName: joi.string().required(),
+    lastName: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
     phone: joi.number().required()
   });
   return schema.validate(data);
@@ -13,8 +13,8 @@ function registerValidation(data) {
 
 function loginValidation(data) {
   const schema = joi.object({
-    email: joi.string().min(6).email().required(),
-    password: joi.string().min(4).required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
   });
   return schema.validate(data);
 }
